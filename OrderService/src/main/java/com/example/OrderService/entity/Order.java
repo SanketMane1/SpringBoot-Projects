7e9f7d5ac8936336d487c8d72dba041679cdc188
SanketMane1 <san56maket@gmail.com>
2023-01-11 05:04:02 +0530

    Order API Created

---
+++ OrderService/src/main/java/com/example/OrderService/entity/Order.java
@@ -0,0 +1,35 @@
+package com.example.OrderService.entity;
+
+import java.time.Instant;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "ORDER_DETAILS")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private long productId;
+	private long quantity;
+	private Instant orderDate;
+	private String orderStatus;
+	private long amount;
+	
+
+}
---
+++ OrderService/src/main/java/com/example/OrderService/service/OrderServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.OrderService.service;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.OrderService.controller.OrderController;
+import com.example.OrderService.entity.Order;
+import com.example.OrderService.model.OrderRequest;
+import com.example.OrderService.repository.OrderRepository;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class OrderServiceImpl implements OrderService {
+	
+	@Autowired
+	OrderRepository orderRepository;
+
+	@Override
+	public long placeOrder(OrderRequest orderRequest) {
+		// TODO Auto-generated method stub
+		
+		log.info("Placing Order {}",orderRequest);
+		
+		Order order = new Order().builder().amount(orderRequest.getTotalAmount())
+											.orderStatus("CREATED")
+											.productId(orderRequest.getProductId())
+											.orderDate(Instant.now())
+											.quantity(orderRequest.getQuantity())
+											.build();
+		order= orderRepository.save(order);
+		log.info("Order placed with order id: {}",order.getId());
+		
+		return order.getId();
+	}
+	
+	
+
+}
