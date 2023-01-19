0bb4f48703d6ecb55601bdfa1ed5b6603aa099c1
SanketMane1 <san56maket@gmail.com>
2023-01-19 12:22:13 +0530

    Added Rate Limiter

--- CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -20,19 +22,11 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 	
+	@Bean
+	KeyResolver userKeySolver() {
+		return exchange -> Mono.just("userKey");
+	}
 	
-	
-//	@Bean
-//	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
-//		return factory -> factory.configureDefault(
-//				id -> new Resilience4JConfigBuilder(id)
-//						.circuitBreakerConfig(
-//								CircuitBreakerConfig.ofDefaults()
-//
-//						)
-//						
-//						.build()
-//		);
-//		}
+
 
 }
